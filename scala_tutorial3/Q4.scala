f7d6c3609f69de8e72b199156d78352e9ad883d8
R Saranjan <108950430+SRKSaran@users.noreply.github.com>
2022-07-18 23:48:47 +0530

    Update Q4.scala
    corrected

--- scala_tutorial3/Q4.scala
+++ scala_tutorial3/Q4.scala
@@ -2,13 +2,22 @@ package scala_tutorial3
 
 object Q4 {
   def main( args: Array[String]): Unit = {
-    println(Wholesale_cost(10))
+    println(Wholesale_cost(60))
+  }
+  def Total_amount(y:Int): Double = {
+    x*y
+  }
+  def Discount(amount: Double): Double = {
+    amount*0.4
+  }
+  def Shipping_cost(y:Int): Double = {
+    if(y>=50){
+      3*50+(y-50)*0.75
+    }else{
+      y*3
+    }
   }
   def Wholesale_cost(y :Int ): Double = {
-    if(y>=50){
-       (24.5*60/100*y)+50*3+(y-50)*0.75
-    }else{
-       (24.5*60/100*y)+(y*3)
-    }
+    Total_amount(y) - Discount(Total_amount(y)) + Shipping_cost(y)
   }
 }
