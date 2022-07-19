b2219c0e59dbb8198e01d13ef7271dcebcdd350f
R Saranjan <108950430+SRKSaran@users.noreply.github.com>
2022-07-19 13:22:57 +0530

    Update Q5.scala

--- scala_tutorial3/Q5.scala
+++ scala_tutorial3/Q5.scala
@@ -2,7 +2,8 @@ package scala_tutorial3
 
 object Q5 {
   def main(args : Array[String]):Unit = {
-    println(Total_run_time())
+    println(easy(2)+tempo(3)+easy(2))
   }
-  def Total_run_time() = {2*8+3*7+2*8}
+  def easy(x:Int) :Int = {x*8}
+  def tempo(y:Int) :Int = {y*7}
 }
